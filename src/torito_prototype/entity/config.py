d40311e67b767726f5492b65036f5d3eef603492
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-06-09 15:29:28 +0900

    configエンティティ中身変更

--- src/torito_prototype/entity/config.py
+++ src/torito_prototype/entity/config.py
@@ -6,20 +6,14 @@ from .bridgeConfig import BridgeConfig
 
 class Config:
     useBridge: bool
-    useDefaultBridge: bool
-    useProxy: bool
-    others: list[str]
     BridgeConfig: BridgeConfig
     ProxyConfig: ProxyConfig
+    others: list[str]
 
-    def __init__(self, useBridge: bool, useDefaultBridge: bool, useProxy: bool, others: list[str], BridgeConfig: BridgeConfig, ProxyConfig: ProxyConfig):
+    def __init__(self, useBridge: bool, BridgeConfig: BridgeConfig, ProxyConfig: ProxyConfig, others: list[str]):
         # validate 
         if not isinstance(useBridge, bool):
             raise ValueError(f"Invalid useBridge: {useBridge}")
-        if not isinstance(useDefaultBridge, bool):
-            raise ValueError(f"Invalid useDefaultBridge: {useDefaultBridge}")
-        if not isinstance(useProxy, bool):
-            raise ValueError(f"Invalid useProxy: {useProxy}")
         if not isinstance(others, list):
             raise ValueError(f"Invalid others: {others}")
         if not isinstance(BridgeConfig, BridgeConfig):
@@ -27,8 +21,6 @@ class Config:
         if not isinstance(ProxyConfig, ProxyConfig):
             raise ValueError(f"Invalid ProxyConfig: {ProxyConfig}")
         self.useBridge = useBridge
-        self.useDefaultBridge = useDefaultBridge
-        self.useProxy = useProxy
         self.others = others
         self.BridgeConfig = BridgeConfig
         self.ProxyConfig = ProxyConfig
