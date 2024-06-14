139d9a32a0f89d4cfe988ac8cbb33d316d2d6f3e
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-06-14 13:19:50 +0900

    CORSへの対応

--- src/torito_prototype/main.py
+++ src/torito_prototype/main.py
@@ -27,7 +27,18 @@ def main():
             return {"message": "torrc saved"}
         except Exception as e:
             return {"message": f"Error: {e}"}
-        
+    
+
+    # CORSへの対応
+    @app.middleware("http")
+    async def add_cors_header(request, call_next):
+        response = await call_next(request)
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "*"
+        return response
+    
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 main()
